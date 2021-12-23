2f181044c6ddf0e7e5a871b786547ae9423cfd41
Nanorock <nano@brightrockgames.com>
2021-12-23 18:17:59 +0000

    Sub 200ms

--- AoCAmphipod23.cs
+++ AoCAmphipod23.cs
@@ -15,17 +15,8 @@ namespace AdventOfCode_2021;
 
 class AoCAmphipod23 : AdventOfCode
 {
-    //15322 !
-
-    public override void Run1()
-    {
-        base.Run1();
-    }
-
-
     public override void Run2()
     {
-        
         var start = ParseBoard();
         var sw = Stopwatch.StartNew();
         var bestMove = GetBestMove(start);
@@ -34,7 +25,7 @@ class AoCAmphipod23 : AdventOfCode
         Console.WriteLine($"Min cost {bestMove} in {sw.Elapsed.TotalMilliseconds}ms");
     }
 
-    List<Board> _possibleMoves = new List<Board>();
+    readonly List<Board> _possibleMoves = new List<Board>(64);
     int GetBestMove(Board initialState)
     {
         var priorityQueue = new PriorityQueue<Board, int>();// Dijkstra
@@ -87,18 +78,19 @@ class AoCAmphipod23 : AdventOfCode
 
     readonly struct Board
     {
-        public readonly ValueArray11<char> Hall;
-        public readonly ValueArray4<string> Rooms;
+        readonly ValueArray11<char> _hall;
+        readonly ValueArray4<string> _rooms;
         public readonly int Cost;
         
-        public override int GetHashCode() => HashCode.Combine(Hall,Rooms);
+        public override int GetHashCode() => HashCode.Combine(_hall,_rooms);
         public Board(ValueArray11<char> hall, ValueArray4<string> rooms, int cost)
         {
-            Hall = hall;
-            Rooms = rooms;
+            _hall = hall;
+            _rooms = rooms;
             Cost = cost;
         }
         
+
         public void GetPossiblesMoves(List<Board> moves)
         {
             GetPossiblesHallToRoomMoves(moves);
@@ -106,20 +98,18 @@ class AoCAmphipod23 : AdventOfCode
                 GetPossiblesRoomMoves(moves);
         }
         
-        
-
         void GetPossiblesHallToRoomMoves(List<Board> moves)
         {
-            for (int i = Hall.Count - 1; i >= 0; --i)
+            for (int i = _hall.Count - 1; i >= 0; --i)
             {
-                var amphipod = Hall[i];
+                var amphipod = _hall[i];
                 if (amphipod == Empty) continue;
                 GetPossiblesHallToRoomMoves(amphipod, i, moves);
             }
         }
         void GetPossiblesRoomMoves(List<Board> moves)
         {
-            for (int i = Rooms.Count - 1; i >= 0; --i)
+            for (int i = _rooms.Count - 1; i >= 0; --i)
             {
                 if (RoomContainsOnlyValid(i)) continue;
                 int roomHallId = _roomHallId[i];
@@ -130,60 +120,64 @@ class AoCAmphipod23 : AdventOfCode
         
         public bool RoomContainsOnlyValid(int id)
         {
-            var r = Rooms[id];
+            var r = _rooms[id];
             var validChar = Amphipods[id];
             for (int i = 0; i < r.Length; i++)
                 if (r[i] != validChar)
                     return false;
             return true;
         }
-        public char PeekRoom(int id) => Rooms[id][^1];
+        public char PeekRoom(int id) => _rooms[id][^1];
         
         void GetPossiblesHallToRoomMoves(char amphipod, int from, List<Board> moves)
         {
             int amphiId = GetAmphiId(amphipod);
+            for (int fwd = from+1; fwd < _hall.Count; fwd++)
+            {
+                if (_hall[fwd] != Empty) break;
+                if (!IsRoomEntrance(fwd)) continue;
+                int roomId = fwd / 2 - 1;
+                if (roomId+1 == amphiId && RoomContainsOnlyValid(roomId)) 
+                    moves.Add(GetRoomMove(amphipod, @from, roomId));
+            }
             for (int rev = from-1; rev >= 0; --rev)
             {
-                if (Hall[rev] != Empty) break;
+                if (_hall[rev] != Empty) break;
                 if (!IsRoomEntrance(rev)) continue;
                 int roomId = rev / 2 - 1;
 
                 if (roomId+1 == amphiId && RoomContainsOnlyValid(roomId)) 
                     moves.Add(GetRoomMove(amphipod, @from, roomId));
             }
-            for (int fwd = from+1; fwd < Hall.Count; fwd++)
-            {
-                if (Hall[fwd] != Empty) break;
-                if (!IsRoomEntrance(fwd)) continue;
-                int roomId = fwd / 2 - 1;
-                if (roomId+1 == amphiId && RoomContainsOnlyValid(roomId)) 
-                    moves.Add(GetRoomMove(amphipod, @from, roomId));
-            }
         }
+
+        static List<int> _possiblePath = new List<int>(16);
         void GetPossiblesRoomHallMoves(char amphipod, int roomHallId, List<Board> moves)
         {
-            for (int rev = roomHallId-1; rev >= 0; --rev)
+            int amphiId = GetAmphiId(amphipod);
+
+            for (int fwd = roomHallId+1; fwd < _hall.Count; ++fwd)
             {
-                if (Hall[rev] != Empty) break;
-                if (!IsRoomEntrance(rev))
-                    moves.Add(GetRoomToHallMove(amphipod, roomHallId, rev));
-                else
-                {
-                    int roomId = rev / 2 - 1;
-                    if (Amphipods[roomId] == amphipod && RoomContainsOnlyValid(roomId))
-                        moves.Add(GetRoomToRoomMove(amphipod, roomHallId, rev));
-                }
-            }
-            for (int fwd = roomHallId+1; fwd < Hall.Count; ++fwd)
-            {
-                if (Hall[fwd] != Empty) break;
+                if (_hall[fwd] != Empty) break;
                 if (!IsRoomEntrance(fwd))
                     moves.Add(GetRoomToHallMove(amphipod, roomHallId, fwd));
                 else
                 {
                     int roomId = fwd / 2 - 1;
-                    if (Amphipods[roomId] == amphipod && RoomContainsOnlyValid(roomId))
+                    if (roomId+1 == amphiId && RoomContainsOnlyValid(roomId))
                         moves.Add(GetRoomToRoomMove(amphipod, roomHallId, fwd));
+                }
+            }
+            for (int rev = roomHallId-1; rev >= 0; --rev)
+            {
+                if (_hall[rev] != Empty) break;
+                if (!IsRoomEntrance(rev))
+                    moves.Add(GetRoomToHallMove(amphipod, roomHallId, rev));
+                else
+                {
+                    int roomId = rev / 2 - 1;
+                    if (roomId+1 == amphiId && RoomContainsOnlyValid(roomId))
+                        moves.Add(GetRoomToRoomMove(amphipod, roomHallId, rev));
                 }
             }
         }
@@ -192,16 +186,15 @@ class AoCAmphipod23 : AdventOfCode
         {
             int amphiId = GetAmphiId(amphipod);
             int moveCost = _amphipodMoveCost[amphiId];
-            var hall = Hall;
+            var hall = _hall;
             hall.Set(from, Empty);
             
-            var rooms = Rooms;    
-            var roomState = Rooms[roomId];
+            var rooms = _rooms;    
+            var roomState = _rooms[roomId];
             roomState += amphipod;
             rooms.Set(roomId, roomState);
 
-            int cost = Math.Abs(from - _roomHallId[roomId]);
-            cost += 5 - roomState.Length;
+            int cost = 5 - roomState.Length + Math.Abs(from - _roomHallId[roomId]);
             cost *= moveCost;
             
             return new Board(hall,rooms, Cost + cost);
@@ -211,17 +204,16 @@ class AoCAmphipod23 : AdventOfCode
             int amphiId = GetAmphiId(amphipod);
             int moveCost = _amphipodMoveCost[amphiId];
 
-            var hall = Hall;
+            var hall = _hall;
             hall.Set(roomHallId, Empty);
             hall.Set(toHallId, amphipod);
             
             int roomId = roomHallId / 2 - 1;
-            var rooms = Rooms;
+            var rooms = _rooms;
             var copyRoomState = rooms[roomId][..^1];
             rooms.Set(roomId, copyRoomState); 
             
-            int cost = 4 - copyRoomState.Length;//Getting to the hallway
-            cost += Math.Abs(roomHallId - toHallId);
+            int cost = 4 - copyRoomState.Length + Math.Abs(roomHallId - toHallId);
             cost *= moveCost;
             
             return new Board(hall, rooms, Cost + cost);
@@ -232,7 +224,7 @@ class AoCAmphipod23 : AdventOfCode
             int moveCost = _amphipodMoveCost[amphiId];
             
             int roomId = roomHallId / 2 - 1;
-            var rooms = Rooms;
+            var rooms = _rooms;
             var copyRoomState = rooms[roomId][..^1];
             rooms.Set(roomId, copyRoomState); 
 
@@ -240,29 +232,24 @@ class AoCAmphipod23 : AdventOfCode
             var copyDestRoomState = rooms[roomId]+amphipod;
             rooms.Set(roomId, copyDestRoomState); 
             
-            int cost = 4 - copyRoomState.Length;//Getting to the hallway
-            cost += 5 - copyDestRoomState.Length;
-            cost += Math.Abs(roomHallId - toHallId);
+            int cost = 9 - copyRoomState.Length- copyDestRoomState.Length + Math.Abs(roomHallId - toHallId);
             cost *= moveCost;
             
-            return new Board(Hall, rooms, Cost + cost);
+            return new Board(_hall, rooms, Cost + cost);
         }
 
         public bool Wins()
         {
-            for (int i = 0; i < Hall.Count; i++)
-                if (Hall[i] != Empty)
+            for (int i = 0; i < _hall.Count; ++i)
+                if (_hall[i] != Empty)
                     return false;
-            for (int i = 0; i < Rooms.Count; i++)
+            for (int i = 0; i < _rooms.Count; ++i)
                 if(!RoomContainsOnlyValid(i))
                     return false;
             return true;
         }
 
-        public override string ToString()
-        {
-            return $"{Hall}/{Rooms[0]}/{Rooms[1]}/{Rooms[2]}/{Rooms[3]}/{Cost}";
-        }
+        public override string ToString() => $"{_hall[0]}{_hall[1]}{_hall[2]}{_hall[3]}{_hall[4]}{_hall[5]}{_hall[6]}{_hall[7]}{_hall[8]}{_hall[9]}{_hall[10]}/{_rooms[0]}/{_rooms[1]}/{_rooms[2]}/{_rooms[3]}/{Cost}";
     }
 
     static int[] _roomHallId = { 2, 4, 6, 8 };
